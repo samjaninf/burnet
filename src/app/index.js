34f11ab90e3a6f3de2e8dff8475fe4fc568a4e37
Feliciano Santana <fsfelicianosantana@gmail.com>
2016-09-10 00:16:45 -0300

    Add index.pug

--- src/app/index.js
+++ src/app/index.js
@@ -1,4 +1,15 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+const appData = require('../../config/application.json');
+
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+
+app.get('/', function(req, res) {
+  res.render('index', {
+    title: appData.title
+  });
+});
 
 module.exports = app;
