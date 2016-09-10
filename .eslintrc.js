9cbe854e727a4929b3cc0acc100654c0b972576b
Feliciano Santana <fsfelicianosantana@gmail.com>
2016-09-09 22:55:01 -0300

    Initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,36 @@
+module.exports = {
+    "extends": "google",
+    "globals": {
+        "angular": true,
+        "chai": true,
+        "sinon": true,
+        "inject": true,
+        "readJSON": true,
+        "expect": true,
+        "google": true
+    },
+    "rules": {
+        'max-len': [1, 110, 4, {
+            ignoreComments: true,
+            ignoreUrls: true
+        }],
+        "quotes": [
+            2,
+            "single"
+        ],
+        "linebreak-style": [
+            2,
+            "unix"
+        ],
+        "semi": [
+            2,
+            "always"
+        ],
+        "no-console": 0,
+        "no-unused-expressions": 0
+    },
+    "env": {
+        "browser": true,
+        "mocha": true
+    }
+};
---
+++ test/middleware/logger.test.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const sinon = require('sinon');
+const chai = require('chai');
+var sinonChai = require('sinon-chai');
+chai.should();
+chai.use(sinonChai);
+
+describe('Logger tests', () => {
+  let app;
+  let logger;
+
+  let infoSpy;
+  let errorSpy;
+
+  const REQ = {};
+  const RES = {};
+  const NEXT = sinon.spy();
+
+  beforeEach(() => {
+    app = {};
+    // We pass an empty object since logger.js sets winston into it
+    logger = require('../../src/middleware/logger.js')(app);
+    infoSpy = sinon.spy(app.logger, 'info');
+    errorSpy = sinon.spy(app.logger, 'error');
+  });
+
+  afterEach(() => {
+    infoSpy.restore();
+    errorSpy.restore();
+  });
+
+  it('should not log anything if no errors are passed to it', () => {
+    const ERROR = null;
+    logger(ERROR, REQ, RES, NEXT);
+    infoSpy.should.not.have.been.called;
+  });
+
+  it('should not append if no error code', () => {
+    const ERROR = {
+      code: null,
+      message: 'failure'
+    };
+    const _REQ = Object.assign({
+      url: 'test'
+    }, REQ);
+    logger(ERROR, _REQ, RES, NEXT);
+    errorSpy.should.have.been.calledWith(`Route: ${_REQ.url} - ${ERROR.message}`);
+  });
+});
